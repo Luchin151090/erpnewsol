94245c840b31d9db89364e67dff4367beda8def2
joseaguasol <151564687+joseaguasol@users.noreply.github.com>
2024-04-30 09:51:58 -0500

    Update logistica_model.py

--- models/logistica_model.py
+++ models/logistica_model.py
@@ -50,8 +50,385 @@ class LogisticaModel:
             if conn:
                 self.db_pool.pool.putconn(conn)
 
-if __name__=="__main__":
-    logistica_model = LogisticaModel()
-    
+    def createMaterial(self,codigo,nombre,descripcion,cantidad,stock,fecha_ingreso):
+        conn = None
+        cursor = None
+        try:
+            conn = self.db_pool.pool.getconn()
+            cursor = conn.cursor()
+            cursor.execute(
+            """
+            INSERT INTO logistica.material (codigo,nombre,descripcion,cantidad,stock,fecha_ingreso) VALUES (%s,%s,%s,%s,%s,%s);
+            """,(codigo,nombre,descripcion,cantidad,stock,fecha_ingreso),True
+            )
+            return 'Material created successfully'
+        except Exception as e:
+            traceback.print_exc()
+            return str(e)
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                self.db_pool.pool.putconn(conn)
         
 
+    def deleteMaterial(self,codigo):
+        conn = None
+        cursor = None
+        try:
+            conn = self.db_pool.pool.getconn()
+            cursor = conn.cursor()
+            cursor.execute(
+            """
+            DELETE FROM logistica.material WHERE codigo = %s;
+            """,(codigo),True
+            )
+            return 'Material deleted successfully'
+        except Exception as e:
+            traceback.print_exc()
+            return str(e)
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                self.db_pool.pool.putconn(conn)
+    
+    def updateMaterial(self,codigo,cantidad,stock):
+        conn = None
+        cursor = None
+        try:
+            conn = self.db_pool.pool.getconn()
+            cursor = conn.cursor()
+            cursor.execute(
+            """
+            UPDATE cars SET cantidad=%s,stock=%s WHERE codigo=%s;
+            """,(cantidad,stock,codigo),True
+            )
+            return 'Material updated successfully'
+        except Exception as e:
+            traceback.print_exc()
+            return str(e)
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                self.db_pool.pool.putconn(conn)
+
+    # TABLA EQUIPO
+    def getEquipo(self):
+        conn = None
+        cursor = None
+        try:
+            conn = self.db_pool.pool.getconn()
+            cursor = conn.cursor()
+
+            query = cursor.execute(
+                """
+                SELECT * FROM logistica.equipo;
+                """
+            )
+            # LISTA PARA RECIBIR LA DATA
+            data = cursor.fetchall()
+
+            datos = list()
+            
+            
+            contenido = dict()
+
+            for row in data:
+                contenido = {
+                    'id':row[0],
+                    'stock':row[1],
+                    'fecha':row[2],
+                    'descripcion':row[3],
+                    'cantidad':row[4],
+                    'codigo':row[5],
+                    'nombre':row[6]
+                }
+                datos.append(contenido)
+                contenido={}
+            return datos
+        except Exception as e:
+            traceback.print_exc()
+            return str(e)
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                self.db_pool.pool.putconn(conn)
+
+    def createEquipo(self,stock,fecha,descripcion,cantidad,codigo,nombre):
+        conn = None
+        cursor = None
+        try:
+            conn = self.db_pool.pool.getconn()
+            cursor = conn.cursor()
+            cursor.execute(
+            """
+            INSERT INTO logistica.equipo (stock,fecha,descripcion,cantidad,codigo,nombre) VALUES (%s,%s,%s,%s,%s,%s);
+            """,(codigo,nombre,descripcion,cantidad,stock,fecha),True
+            )
+            return 'Equipo created successfully'
+        except Exception as e:
+            traceback.print_exc()
+            return str(e)
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                self.db_pool.pool.putconn(conn)
+        
+
+    def deleteEquipo(self,codigo):
+        conn = None
+        cursor = None
+        try:
+            conn = self.db_pool.pool.getconn()
+            cursor = conn.cursor()
+            cursor.execute(
+            """
+            DELETE FROM logistica.equipo WHERE codigo = %s;
+            """,(codigo),True
+            )
+            return 'Equipo deleted successfully'
+        except Exception as e:
+            traceback.print_exc()
+            return str(e)
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                self.db_pool.pool.putconn(conn)
+    
+    def updateEquipo(self,codigo,cantidad,stock):
+        conn = None
+        cursor = None
+        try:
+            conn = self.db_pool.pool.getconn()
+            cursor = conn.cursor()
+            cursor.execute(
+            """
+            UPDATE logistica.equipo SET cantidad=%s,stock=%s WHERE codigo=%s;
+            """,(cantidad,stock,codigo),True
+            )
+            return 'Equipo updated successfully'
+        except Exception as e:
+            traceback.print_exc()
+            return str(e)
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                self.db_pool.pool.putconn(conn)
+
+    # TABLA SUBAREA
+    def getSubarea(self):
+        conn = None
+        cursor = None
+        try:
+            conn = self.db_pool.pool.getconn()
+            cursor = conn.cursor()
+
+            query = cursor.execute(
+                """
+                SELECT * FROM logistica.subarea;
+                """
+            )
+            # LISTA PARA RECIBIR LA DATA
+            data = cursor.fetchall()
+
+            datos = list()
+            
+            
+            contenido = dict()
+
+            for row in data:
+                contenido = {
+                    'id':row[0],
+                    'nombre':row[1],
+                    'responsable':row[2]
+                }
+                datos.append(contenido)
+                contenido={}
+            return datos
+        except Exception as e:
+            traceback.print_exc()
+            return str(e)
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                self.db_pool.pool.putconn(conn)
+
+    def createSubarea(self,nombre,responsable):
+        conn = None
+        cursor = None
+        try:
+            conn = self.db_pool.pool.getconn()
+            cursor = conn.cursor()
+            cursor.execute(
+            """
+            INSERT INTO logistica.subarea (nombre,responsable) VALUES (%s,%s);
+            """,(nombre,responsable),True
+            )
+            return 'Subarea created successfully'
+        except Exception as e:
+            traceback.print_exc()
+            return str(e)
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                self.db_pool.pool.putconn(conn)
+        
+
+    def deleteSubarea(self,nombre):
+        conn = None
+        cursor = None
+        try:
+            conn = self.db_pool.pool.getconn()
+            cursor = conn.cursor()
+            cursor.execute(
+            """
+            DELETE FROM logistica.subarea WHERE nombre = %s;
+            """,(nombre),True
+            )
+            return 'Subarea deleted successfully'
+        except Exception as e:
+            traceback.print_exc()
+            return str(e)
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                self.db_pool.pool.putconn(conn)
+    
+    def updateSubarea(self,responsable,nombre):
+        conn = None
+        cursor = None
+        try:
+            conn = self.db_pool.pool.getconn()
+            cursor = conn.cursor()
+            cursor.execute(
+            """
+            UPDATE logistica.subarea SET responsable=%s WHERE nombre=%s;
+            """,(responsable,nombre),True
+            )
+            return 'Subarea updated successfully'
+        except Exception as e:
+            traceback.print_exc()
+            return str(e)
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                self.db_pool.pool.putconn(conn)
+    
+    #TABLA FVAL
+    def getFval(self):
+        conn = None
+        cursor = None
+        try:
+            conn = self.db_pool.pool.getconn()
+            cursor = conn.cursor()
+
+            query = cursor.execute(
+                """
+                SELECT * FROM logistica.fval;
+                """
+            )
+            # LISTA PARA RECIBIR LA DATA
+            data = cursor.fetchall()
+
+            datos = list()
+            
+            
+            contenido = dict()
+
+            for row in data:
+                contenido = {
+                    'id':row[0],
+                    'nombre':row[1],
+                    'cantidad':row[2],
+                    'fecha_req':row[3],
+                    'solicitante':row[4],
+                    'area_solicitante':row[5]
+                }
+                datos.append(contenido)
+                contenido={}
+            return datos
+        except Exception as e:
+            traceback.print_exc()
+            return str(e)
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                self.db_pool.pool.putconn(conn)
+
+    def createFval(self,nombre,cantidad,fecha_req,solicitante,area_solicitante):
+        conn = None
+        cursor = None
+        try:
+            conn = self.db_pool.pool.getconn()
+            cursor = conn.cursor()
+            cursor.execute(
+            """
+            INSERT INTO logistica.fval (nombre,cantidad,fecha_req,solicitante,area_solicitante) VALUES (%s,%s,%s,%s,%s);
+            """,(nombre,cantidad,fecha_req,solicitante,area_solicitante),True
+            )
+            return 'Fval created successfully'
+        except Exception as e:
+            traceback.print_exc()
+            return str(e)
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                self.db_pool.pool.putconn(conn)
+        
+
+    def deleteFval(self,nombre):
+        conn = None
+        cursor = None
+        try:
+            conn = self.db_pool.pool.getconn()
+            cursor = conn.cursor()
+            cursor.execute(
+            """
+            DELETE FROM logistica.fval WHERE nombre = %s;
+            """,(nombre),True
+            )
+            return 'Fval deleted successfully'
+        except Exception as e:
+            traceback.print_exc()
+            return str(e)
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                self.db_pool.pool.putconn(conn)
+    
+    def updateFval(self,cantidad,nombre):
+        conn = None
+        cursor = None
+        try:
+            conn = self.db_pool.pool.getconn()
+            cursor = conn.cursor()
+            cursor.execute(
+            """
+            UPDATE logistica.fval SET cantidad=%s WHERE nombre=%s;
+            """,(cantidad,nombre),True
+            )
+            return 'Fval updated successfully'
+        except Exception as e:
+            traceback.print_exc()
+            return str(e)
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                self.db_pool.pool.putconn(conn)
+
+if __name__=="__main__":
+    logistica_model = LogisticaModel()
