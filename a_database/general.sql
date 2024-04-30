a04251ab7a303d3daf36043b3e18bc4016a0f428
joseaguasol <151564687+joseaguasol@users.noreply.github.com>
2024-04-30 09:45:55 -0500

    Update and rename erp.sql to general.sql

---
+++ a_database/general.sql
@@ -0,0 +1,12 @@
+-- SCHEMAS
+CREATE SCHEMA logistica;
+CREATE SCHEMA contabilidad;
+CREATE SCHEMA area;
+CREATE SCHEMA relaciones;
+CREATE SCHEMA personal;
+CREATE SCHEMA calidad;
+CREATE SCHEMA administracion;
+CREATE SCHEMA mantenimiento;
+CREATE SCHEMA produccion;
+CREATE SCHEMA comercial;
+CREATE SCHEMA finanzas;
