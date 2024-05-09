a9706ca24695f961d9ade505a1f18bbb68e230eb
joseaguasol <josemsancheza14@gmail.com>
2024-05-09 11:17:55 -0500

    alter table finanzas

--- a_database/finanzas.sql
+++ a_database/finanzas.sql
@@ -1,7 +1,7 @@
 CREATE TABLE area.area_finanzas(
 	id serial primary key,
 	nombre varchar(200),
-	responsable_id int
+    area_id int
 );
 
 CREATE TABLE finanzas.control_asistencia(
@@ -10,14 +10,16 @@ CREATE TABLE finanzas.control_asistencia(
     hora_salida time,
     fecha date,
     nombre_empleado varchar(200),
-    cantidad_horas int
+    cantidad_horas int,
+    area_finanzas_id int
 );
 
 CREATE TABLE finanzas.fvac(
     id serial primary key,
     monto_ingreso float,
     monto_salida float,
-    fecha date
+    fecha date,
+    area_finanzas_id int
 );
 
 CREATE TABLE finanzas.caja(
@@ -25,7 +27,24 @@ CREATE TABLE finanzas.caja(
     nombre_caja varchar(200),
     saldo_final float,
     saldo_inicial float,
-    caja_id int
+    caja_id int,
+    area_finanzas_id int
 );
 
---ALTER TABLES
+CREATE TABLE finanzas.transaccion(
+    id serial primary key,
+    fecha_transaccion date,
+    monto float,
+    caja_origen int,
+    caja_destino int
+);
+
+-- ALTER TABLES AREA
+ALTER TABLE area.area_finanzas ADD CONSTRAINT fk_area_finanzas_area FOREIGN KEY (area_id) REFERENCES area.area (id) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- ALTER TABLES LOGISTICA
+ALTER TABLE finanzas.control_asistencia ADD CONSTRAINT fk_control_asistencia_finanzas FOREIGN KEY (area_finanzas_id) REFERENCES area.area_finanzas (id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE finanzas.fvac ADD CONSTRAINT fk_fvac_finanzas FOREIGN KEY (area_finanzas_id) REFERENCES area.area_finanzas (id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE finanzas.caja ADD CONSTRAINT fk_caja_finanzas FOREIGN KEY (area_finanzas_id) REFERENCES area.area_finanzas (id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE finanzas.transaccion ADD CONSTRAINT fk_transaccion_origen FOREIGN KEY(caja_origen) REFERENCES finanzas.caja (id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE finanzas.transaccion ADD CONSTRAINT fk_transaccion_destino FOREIGN KEY(caja_destino) REFERENCES finanzas.caja (id) ON DELETE CASCADE ON UPDATE CASCADE;
